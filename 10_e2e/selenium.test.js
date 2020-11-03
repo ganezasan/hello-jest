192cf0ad37dc4f2091025924828e141ed8fe3e8f
ganezasan <istaka12@gmail.com>
2020-11-03 23:11:22 +0900

    add selenium test

---
+++ 10_e2e/selenium.test.js
@@ -0,0 +1,54 @@
+import { Builder, By, Key, until, Capabilities } from 'selenium-webdriver';
+
+describe('e2e test', () => {
+  let chromeDriver = {};
+  let geckoDriver = {};
+
+  beforeAll(async () => {
+    const chromeCapabilities = Capabilities.chrome();
+    const fireFoxCapabilities = Capabilities.firefox();
+    chromeCapabilities.set('goog:chromeOptions', {
+      args: [
+        '--headless',
+        '--no-sandbox',
+        '--disable-gpu',
+        '--lang=en-US',
+        // '--user-data-dir=./tmp_user_data', //--headlessを外す場合は有効化する
+      ],
+    });
+
+    fireFoxCapabilities.set('moz:firefoxOptions', {
+      args: [
+        '-headless',
+      ],
+    });
+
+    chromeDriver = await new Builder().withCapabilities(chromeCapabilities).build();
+    geckoDriver = await new Builder().withCapabilities(fireFoxCapabilities).build();
+  });
+
+  afterAll(async () => {
+    await chromeDriver.quit();
+    await geckoDriver.quit();
+  });
+
+  it('When using Chrome, the search keyword would be the title in the page on google.com', async () => {
+    // google.comにアクセス
+    await chromeDriver.get('https://www.google.com/ncr');
+    // 検索ボックスの要素を探し、webdriver`、エンターキーを入力
+    await chromeDriver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+    // ページのタイトルが`webdriver - Google Search`に切り替わるまで待つ
+    const results = await chromeDriver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    expect(results).toBe(true);
+  });
+
+  it('When using Firefox, the search keyword would be the title on the page in google.com', async () => {
+    // google.comにアクセス
+    await geckoDriver.get('https://www.google.com/ncr');
+    // 検索ボックスの要素を探し、webdriver`、エンターキーを入力
+    await geckoDriver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+    // ページのタイトルが`webdriver - Google Search`に切り替わるまで待つ
+    const results = await geckoDriver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    expect(results).toBe(true);
+  });
+});
