8c28033053cc19cc97d494a86235ffd4e2bef835
ganezasan <istaka12@gmail.com>
2020-12-13 23:01:04 +0900

    add comments

--- 08_mock/mock.test.js
+++ 08_mock/mock.test.js
@@ -1,14 +1,14 @@
 describe('#mock', () => {
   it('Check `jest.fn()` specification', () => {
     const mockFunction = jest.fn();
-    expect(mockFunction()).toBe(undefined);
-    expect(mockFunction).toHaveProperty('mock');
-    expect(mockFunction.mock.calls.length).toBe(1);
-    expect(mockFunction.mock.calls[0]).toEqual([]);
-    expect(mockFunction.mock.results.length).toBe(1);
-    expect(mockFunction.mock.results[0].type).toBe('return');
-    expect(mockFunction.mock.results[0].value).toBe(undefined);
-    expect(mockFunction.mock.instances[0]).toBe(undefined);
+    expect(mockFunction()).toBe(undefined); // mockFunction関数の結果は`undefined`である
+    expect(mockFunction).toHaveProperty('mock'); // mockFunction関数はmockプロパティを持っている
+    expect(mockFunction.mock.calls.length).toBe(1); // mockFunction関数は1度呼び出された
+    expect(mockFunction.mock.calls[0]).toEqual([]); // mockFunction関数が1度呼び出された際に、引数は空だった
+    expect(mockFunction.mock.results.length).toBe(1); // mockFunction関数の結果は1つある
+    expect(mockFunction.mock.results[0].type).toBe('return'); // mockFunction関数が1度目に呼び出された結果は正常にリターンされている
+    expect(mockFunction.mock.results[0].value).toBe(undefined); // mockFunction関数の1度目の結果は`undefined`である
+    expect(mockFunction.mock.instances[0]).toBe(undefined); // mockFunction関数からnewを利用してインスタンスを作成していない
   });
   it('return `Hello`', () => {
     const mockFunction = jest.fn().mockImplementation(() => 'Hello');
